8489fda273b280446123417ae3096484fe7af980
shreyasbabu <shreyasbabu@users.noreply.github.com>
2021-02-24 18:39:07 -0500

    added cinemachine directional movement with attak and hit animations added

--- Assets/Scripts/MainCharacter/MainCharacterController.cs
+++ Assets/Scripts/MainCharacter/MainCharacterController.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class MainCharacterController : MonoBehaviour
 {
+    [SerializeField]
+    private float m_speed = 0.0f;
+    private Vector3 m_input = Vector3.zero;
+    private Animator m_CharacterAnimator;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(GetComponent<Animator>())
+        {
+            m_CharacterAnimator = GetComponent<Animator>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Attack Logic
+        if(Input.GetKey(KeyCode.Space))
+        {
+            m_CharacterAnimator.SetTrigger("Attack");
+            return;
+        }
+        //Move Logic
+        m_input.x = Input.GetAxis("Horizontal");
+        m_input.y = Input.GetAxis("Vertical");
+        if(m_input.x < 0.0f)
+        {
+            gameObject.transform.rotation = new Quaternion(0, 180, 0, 0);
+        }
+        else
+        {
+            gameObject.transform.rotation = new Quaternion(0, 0, 0, 0);
+
+        }
+        m_CharacterAnimator.SetFloat("Speed", m_input.sqrMagnitude);
+
+
+        transform.position += m_input * m_speed * Time.deltaTime;
     }
 }
